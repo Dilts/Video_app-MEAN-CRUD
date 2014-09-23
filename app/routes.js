c1943940832e2e5132bb176c7ce077ca3d52080e
Brian Dilts <BrianDilts@ADMINs-MacBook-Pro.local>
2014-09-23 16:37:29 -0600

    users now displayed and delete-able, REGEX not working, Update not finished

--- app/routes.js
+++ app/routes.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
 // ________________API______________________
 
 // Display all users in the DB
-	app.get('/api/user', function(req, res){
+	app.get('/api/users', function(req, res){
 		User.find(function(err, users){
 			if (err)
 				res.send(err);
@@ -52,6 +52,21 @@ module.exports = function(app) {
 		);
 	});
 
+	app.put('/api/users/:user_id', function (req,res){
+
+		User.findOneAndUpdate(req.params._id, function(err, user){
+				if(err)
+					return res.send(err)
+				console.log('user to change', user)
+				user.name = req.body.name;
+				user.save(function(err){
+					if (err)
+						return res.send(err)
+					res.json(user);
+				})
+			}
+		);
+	});
 
 
 
@@ -62,69 +77,3 @@ module.exports = function(app) {
 
 }
 
-
-
-
-
-// var indexController = {
-
-// exports.updateUser = function(req, res){
-// 	console.log('Body is', req.body)
-
-// 	var user2 = req.body;
-// 	console.log(user2.name)
-
-// 	user.findOneAndUpdate({name: user2.name},user2, function(err, update){
-// 	if(err){
-// 		return res.send(500, err);
-
-// 	}
-
-// 	if(!update){
-// 		return res.send(404);
-// 	}
-
-// 	return res.send(200, update);
-// 	});
-// }
-
-// };
-
-// module.exports = indexController;a
-
-
-
-// var indexController = {
-// 	index: function(req, res) {
-// 		res.render('index');
-// 	},
-
-// 	createUpload: function(req, res) {
-// 	console.log(req.body)
-
-
-//     var user = new userModel({
-// 	    name: req.body.name,
-// 	    url: req.body.url,
-// 	    title: req.body.title,
-// 	    desc: req.body.desc,
-// 	    });  	
-    
-//     // req.user.name = req.body.name,
-//     // req.user.url = req.body.url,
-//     // req.user.title = req.body.title,
-//     // req.user.desc = req.body.desc,
-
-//     console.log(user)
-
-//     user.create(user, function(err, doc) {
-//     	if (err) {
-//     		console.log('error not saving', err)
-//     		res.send(404, 'did not save')
-//     	}
-//     	res.send(200)
-//     }) 
-
-//     // res.render('index', req)
-
-// 	},
--- public/js/core.js
+++ public/js/core.js
@@ -17,7 +17,7 @@ videos.factory('videoService', function ($http) {
 				});
 		},
 		getUser: function(name, callback) {
-			$http.get('/api/users/' + name).success(function(data)
+			$http.get('/api/users' + name).success(function(data)
 				{ return callback(data);
 				}).error(function(data) {
 					return callback(data);
@@ -29,9 +29,23 @@ videos.factory('videoService', function ($http) {
 				}).error(function(data) {
 					return callback(data)
 				});
+		},
+		updateUser:function(id, callback) {
+			$http.put('/api/users/' + id).success(function(data)
+				{ console.log('grab from the server:', data);
+					return callback(data);
+				}).error(function(data) {
+					return callback(data);
+				});
+		},
+		listUsers: function(callback) {
+			$http.get('/api/users').success(function(data) 
+				{ console.log('list out users:', data);
+				return callback(data);
+			});
 		}
-	}
-});
+}});
+
 
 
 
@@ -49,16 +63,23 @@ videos.controller('mainController', ['$scope', '$http', '$q', 'videoService', fu
 	$scope.formData.title = null;
 	$scope.formData.desc = null;
 
+	$scope.editData = {};
+	$scope.edit = [];
+	$scope.editData.name = null;
+	$scope.editData.url = null;
+	$scope.editData.title = null;
+	$scope.editData.desc = null;
 
 	$scope.createUser = function() {
 		var userDefer = $q.defer();
-		$scope.formData.url = $scope.formData.url.replace('regex from refactoru')
+		// $scope.formData.url = $scope.formData.url.replace('/(?:[https?:]*\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=|embed\/)?(\w+)(?:.*)/gi');
 		videoService.addUser($scope.formData, function(user){
+
 			if(user) {
 				console.log('this has come back from services: ', user);
 				// $scope.name = {};
 				$scope.users.push(user);
-				console.log('did we push here:', $scope.users)
+				console.log('did I push it here:', $scope.users)
 				userDefer.resolve();
 			}
 			else {
@@ -68,21 +89,9 @@ videos.controller('mainController', ['$scope', '$http', '$q', 'videoService', fu
 		return userDefer.promise;
 	};
 
-	$scope.saveChanges = function() {
-		console.log($scope.userId, $scope.selectedUser);
-		$http({ method: 'PUT', url: 'http://localhost:6189/users/', data: $scope.selectedUser}).
-		                    success(function (data, status, headers, config) {
-		                        console.log('sucess', data);
-		                       
-		                    }).
-		                    error(function (data, status, headers, config) {
-		                        console.log(data, status);
-		                    });
-
-	};
 
 	$scope.removeUser = function(id) {
-		console.log('id parameter ', id);
+		// console.log('id parameter ', id);
 		console.log('users before splice: ', $scope.users)
 		for (var i = 0; i < $scope.users.length; i++) {
 			if ($scope.users[i]._id === id) {
@@ -95,6 +104,36 @@ videos.controller('mainController', ['$scope', '$http', '$q', 'videoService', fu
 
 		});
 	};
+
+	$scope.editUser = function(id) {
+		console.log('id parameter ', id);
+		// debugger;
+		videoService.updateUser($scope.user, function(user){
+			
+		})
+	};
+
+
+	$scope.displayUsers = function() {
+		
+		videoService.listUsers( function(users) {
+			$scope.users = users;
+		})
+	};
+
+	// $scope.saveChanges = function(id) {
+	// 	console.log($scope.userId, $scope.users);
+	// 	$http({ method: 'PUT', url: 'http://localhost:6189/users/', data: $scope.users}).
+	// 	                    success(function (data, status, headers, config) {
+	// 	                        console.log('success', data);
+		                       
+	// 	                    }).
+	// 	                    error(function (data, status, headers, config) {
+	// 	                        console.log(data, status);
+	// 	                    });
+
+	// };
+
 
 }]);
 
